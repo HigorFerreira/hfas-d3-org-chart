44d720f910e753dc40ee9a5f71f9c1277f7bbe32
Higor Ferreira <hfashigor@hotmail.com>
2026-01-28 11:37:14 -0300

    BUNDLING PACKAGE

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: {
+		index: "src/index.ts",
+	},
+	format: ["esm", "cjs"],
+	dts: true,
+	sourcemap: true,
+	clean: true,
+	splitting: false,
+	treeshake: true,
+
+	// IMPORTANT: bundle dependencies like d3-org-chart into the output
+	// tsup (esbuild) often externalizes deps automatically; force-bundle these:
+	// noExternal: ["d3-org-chart", "d3-hierarchy", "d3-selection", "d3-shape", "d3-zoom"],
+	// If your OrgChart pulls other d3 subpackages, add them too.
+});
